0f9c7f0ae94156e78fc255a05360c7cee25a4f00
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-10 01:26:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -689,7 +689,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('>bcall')){
- if(!message.author.id === '349616310734553088') return;
+ if(!message.author.id === '396958215377780747') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
