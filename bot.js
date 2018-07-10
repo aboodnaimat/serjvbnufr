f02bd1967baa815b54f0ffabd93c1f83027bf78f
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-10 13:49:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2392,7 +2392,7 @@ if (message.content.startsWith(prefix + "profile")) {
           var client = message.author;
  
       }
-if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: "HypeLC User", rep: 0, lastDaily: "NOT COLLECTED"};
+if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: "SuperBot User", rep: 0, lastDaily: "NOT COLLECTED"};
             let Image = Canvas.Image,
             canvas = new Canvas(300, 300),
             ctx = canvas.getContext('2d');
