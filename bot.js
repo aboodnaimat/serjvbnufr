50b6b8f0f666c3e56ae6720fe9062d3c4d490443
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-13 21:45:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2206,7 +2206,7 @@ client.on('message', message => {
 
  
 
-let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
+let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 client.on("message", message => {
  
   if (message.author.bot) return;
