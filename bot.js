0ff491f85f934c2359c0b9629430f50b6c4ba56a
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-17 08:13:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2141,7 +2141,7 @@ client.on('message', message => {
 });
 
 const iscord = require('discord.js');
-const client = new Discord.Client();
+const lient = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
 client.on("guildMemberAdd", member => {
