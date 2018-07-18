ea143ea3d0682f6401ee1fabc6a5dac67b5bdd68
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-18 13:31:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2382,7 +2382,6 @@ if (message.content.startsWith(prefix + "profile")) {
  
       }
 if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: "Super User", rep: 0, lastDaily: "NOT COLLECTED"};
-            let Image = Image,
             canvas = new Canvas(300, 300),
             ctx = canvas.getContext('2d');
             fs.readFile("Super.png", function (err, Background) { //امتداد الصورة
