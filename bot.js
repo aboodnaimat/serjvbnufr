7e7946022a47630239ec4643d3c1db3aca5855ac
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-26 10:05:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3158,20 +3158,20 @@ client.on("guildMemberAdd", member => {
                                     ctx.font = '24px kathen';
                               ctx.fontSize = '25px';
                               ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(member.user.username, 210, 175);
+                                ctx.fillText(member.user.username, 250, 140);
                               
                               //NAMEً
                               ctx.font = '24px kathen';
                               ctx.fontSize = '20px';
                               ctx.fillStyle = "#FFFFFF";
-      ctx.fillText(`Welcome To ${member.guild.name}`, 210, 100);
+      ctx.fillText(`Welcome To ${member.guild.name}`, 250, 70);
       
                               //AVATARً
                               let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                 ctx.arc(150.8, 150.5, 103.3, 0, Math.PI*2, true);
+                 ctx.arc(130.8, 130.5, 103.3, 0, Math.PI*2, true);
                    ctx.closePath();
                    
                                  ctx.clip();
