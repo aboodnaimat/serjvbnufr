0fefdbb90f540bf40cb023bdb89f924cf9400e62
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-26 13:00:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3171,7 +3171,7 @@ client.on("guildMemberAdd", member => {
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                 ctx.arc(80.8, 80.5, 70.3, 0, Math.PI*2, true);
+                 ctx.arc(80.8, 100.5, 80.3, 0, Math.PI*2, true);
                    ctx.closePath();
                    
                                  ctx.clip();
